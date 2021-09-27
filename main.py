e027c554d0de46153de734ce0319968025b2bb84
LukeDude200 <croslynlc@gmail.com>
2021-09-27 14:57:55 -0300

    Fixing file and start reading file

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Maybe do a city object with an ID and an array that has the distances to every other city?
 # Need to read in the file and put it in an adjacency matrix
 
-CityData = open("Sample Cities (Size 15).txt", "r")
-TestMatrix = [[0 for x in range(15)] for y in range(15)]
-print(TestMatrix)
+from CityObject import City
+cityArray = []
+cityData = open("Sample Cities (Size 15).txt", "r")
+testMatrix = [[0 for x in range(15)] for y in range(15)]
+for i in range(testMatrix):
+    cityArray.append(City())
+for i in range(testMatrix):
+    cityData.readline()
+
+print(testMatrix)
