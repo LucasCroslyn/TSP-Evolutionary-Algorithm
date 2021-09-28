6ad0f7586a267bdba4b5b3507c241a6749e68a9c
LukeDude200 <croslynlc@gmail.com>
2021-09-28 16:15:27 -0300

    Fixing file I/O and putting city data in

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 
 from CityObject import City
 cityArray = []
+temp = ""
+TestCity = City("Test", 1, [])
 cityData = open("Sample Cities (Size 15).txt", "r")
-testMatrix = [[0 for x in range(15)] for y in range(15)]
-for i, j in testMatrix:
-    cityArray.append(City("City" + i, i, []))
-for i, j in testMatrix:
-    cityData.readline()
-
-print(testMatrix)
-print(cityArray)
+#testMatrix = [[0 for x in range(15)] for y in range(15)]
+NUMBER_OF_CITIES = 15
+for i in range(NUMBER_OF_CITIES):
+    distancesPerCity = cityData.readline()
+    cityArray.append(City("City" + str(i), i, distancesPerCity.split()))
+print(cityArray[14].distances)
