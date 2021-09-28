e8e5024ad4610ec91e25a3a47dace0819773ed0e
LukeDude200 <croslynlc@gmail.com>
2021-09-28 16:38:31 -0300

    initialize function made

--- Functions.py
+++ Functions.py
@@ -1,5 +1,12 @@
 from CityObject import City
 
 
+def initialize(numCities, dataFile, cityArray):
+    cityData = open(dataFile, "r")
+    for i in range(numCities):
+        distancesPerCity = cityData.readline()
+        cityArray.append(City("City" + str(i), i, distancesPerCity.split()))
+
+
 def swap(cityArray, int1, int2):
     cityArray[int1], cityArray[int2] = cityArray[int2], cityArray[int1]
